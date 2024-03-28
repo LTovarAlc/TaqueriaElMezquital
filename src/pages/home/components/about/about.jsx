c8abc6dc214c5f5381b1117b23343b4c6d1884fb
Luis Tovar <ltovaralc@gmail.com>
2024-03-27 19:24:22 -0600

    add section about

---
+++ src/pages/home/components/about/about.jsx
@@ -0,0 +1,19 @@
+import "./about.css";
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2 className="about__title">Acerca de nosotros</h2>
+      <div className="about__text-container">
+        <p className="about__text">
+          La taquería "El Mezquital" espera para ser tu nuevo destino para
+          disfrutar de deliciosos tacos de barbacoa y carne asada. Nos
+          enorgullece ofrecer una experiencia unica, con taquitos que
+          seguramente satisfarán tus antojos más exigentes.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,11 +4,13 @@ import Header from "../../components/header/header"
 import Location from "./components/location/location"
 import Reviews from "./components/reviews/reviews"
 import Footer from "../../components/footer/footer"
+import About from "./components/about/about"
 
 const Home = () => {
     return(
         <section className="home">
             <Header/>
+            {/* <About/> */}
             <Location/>
             <Reviews/>
             <Footer/>
