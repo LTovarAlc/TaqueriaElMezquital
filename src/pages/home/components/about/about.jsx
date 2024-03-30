1e53a5d0568d43e5d5645831c343d3102839ad49
Luis Tovar <ltovaralc@gmail.com>
2024-03-29 23:32:31 -0600

    add

--- src/pages/home/components/about/about.jsx
+++ src/pages/home/components/about/about.jsx
@@ -3,14 +3,15 @@ import "./about.css";
 const About = () => {
   return (
     <section className="about">
-      <h2 className="about__title">Acerca de nosotros</h2>
+      <h2 className="section__title">Nosotros</h2>
       <div className="about__text-container">
         {/* <img src="../../../../img/taco.png" alt="" className="taco" /> */}
         <p className="about__text">
-          Nuestro local "El Mezquital" espera para ser tu nuevo destino para
-          disfrutar de deliciosos tacos de barbacoa y carne asada. Nos
-          enorgullece ofrecer una experiencia única, con taquitos que
-          seguramente satisfarán tus antojos más exigentes.
+          Nosotros "El Mezquital" te garantizamos una experiencia única con deliciosos taquitos que deleitaran tus antojos más exigentes
+          <br />
+          <br />
+          Ven y comprueba la calidad de nuestros tacos de Barbacoa y carne
+          asada, estamos seguros que te sorprenderá su delicioso sabor
         </p>
       </div>
     </section>
