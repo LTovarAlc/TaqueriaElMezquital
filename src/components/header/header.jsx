bd6bb5eac09de9590a2f17ce6b7d408c21e745a0
Luis Tovar <ltovaralc@gmail.com>
2024-03-29 22:36:52 -0600

    header update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './header.css';
 
 const Header = () => {
+    const [headerHeight, setHeaderHeight] = useState('20vh');
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
+            if (scrollPercentage >= 2) {
+                setHeaderHeight('10vh');
+            } else {
+                setHeaderHeight('20vh');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <header className="header">
-            <img src="../../../../img/mezquitalLOGO.png" className='elmezquitalLogo' />
+        <header className="header" style={{ height: headerHeight }}>
+            <img src="../../../../img/TacosElMezquitalLOGO.png" className='elmezquitalLogo' />
         </header>
     );
 };
