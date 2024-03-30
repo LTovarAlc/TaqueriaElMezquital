c13554538ca0b2e5bba2c52df8a4ec04a07457b2
Luis Tovar <ltovaralc@gmail.com>
2024-03-30 00:02:36 -0600

    edit footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css"
 const Footer = () => {
     return( 
         <section className="footer">
-            <img src="../../../../img/mezquitalLOGO.png" className="elmezquitalLogo__footer" />
+            <img src="../../../../img/TacosElMezquitalLOGO.png" className="elmezquitalLogo__footer" />
             <span> Todos los derechos reservados &copy; </span>
         </section>
     )
